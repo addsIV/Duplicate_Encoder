5989afca5027dcf912a9ede0401b0989e8ed5b9d
Kevin Lin using laptop <kl13245768@gmail.com>
2022-01-22 10:42:09 +0800

    1510

--- Duplicate_Encoder/leet.cs
+++ Duplicate_Encoder/leet.cs
@@ -14,6 +14,24 @@ namespace Duplicate_Encoder
 
 		public class LeetCode_Solution
 		{
+			public bool WinnerSquareGame(int n)//1510
+			{
+				bool[] dp = new bool[n + 1];
+
+				for (int i = 1; i <= n; i++)
+				{
+					for (int k = 1; k * k <= i; k++)
+					{
+						if (!dp[i - k * k])
+						{
+							dp[i] = true;
+							break;
+						}
+					}
+				}
+
+				return dp[n];
+			}
 			public int MinEatingSpeed(int[] piles, int h)//875
 			{
 				long left = 1, right = 0;
